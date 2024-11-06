ef3c7c0e70c674e228e31abc325113ea3ede5d14
Rahul Mahto <Rahulmahto837@gmail.com>
2024-11-06 10:56:28 +0530

    Issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { store, persistor } from "./redux/store";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 // import { Tooltip } from "./components/tooltip";
-import { ThemeProvider } from "./components/theme";
+// import { ThemeProvider } from "./components/theme";
 import { OnlineStatus } from "./components/onlineStatus";
 import { ScrollToTopButton } from "./components/scrollToTop";
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,13 +13,13 @@ function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-        <ThemeProvider>
+        {/* <ThemeProvider> */}
           <Outlet />
           {/* <Tooltip id="my-tooltip" /> */}
           <ScrollToTopButton />
           <Toaster />
           <OnlineStatus />
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </PersistGate>
     </Provider>
   );
