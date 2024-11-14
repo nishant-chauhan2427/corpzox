f86423a2b1a929ab0304388ff28bcb5a01dcbba3
vinayak-vayuz <vinayak.tripathi@vayuz.com>
2024-11-14 18:42:42 +0530

    added tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-// import { Tooltip } from "./components/tooltip";
+import { Tooltip } from "./components/tooltip";
 // import { ThemeProvider } from "./components/theme";
 import { OnlineStatus } from "./components/onlineStatus";
 import { ScrollToTopButton } from "./components/scrollToTop";
