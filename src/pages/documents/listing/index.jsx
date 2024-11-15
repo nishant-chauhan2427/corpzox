13ef2842bb333b92bea83426cb8581d68490b424
vinayak-vayuz <vinayak.tripathi@vayuz.com>
2024-11-15 17:50:49 +0530

    added documments

--- src/pages/documents/listing/index.jsx
+++ src/pages/documents/listing/index.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { LinkButton } from "../../../components/link";
 import { IoMdAddCircle } from "react-icons/io";
 import { Search } from "../../../components/search";
 import { Selector } from "../../../components/select";
 
 const DocumentsListing = () => {
-  const fileTypeOptions = [
-    { label: "Documents", value: "documents" },
-    { label: "Image", value: "image" },
+  const servicesOptions = [
+    { label: "Fractional CFO Services", value: "fcs" },
+    { label: "Sole Proprietorship", value: "sc" },
   ];
+
+  const folders = [
+    {
+      name: "Folder",
+      files: [
+        { name: "Sample Image", url: "/icons/documents/image-sample.svg", type: "image" },
+        { name: "Sample Video", url: "/icons/documents/video-sample.mp4", type: "video" },
+        { name: "Sample PDF", url: "/icons/documents/pdf-sample.pdf", type: "pdf" },
+      ],
+    },
+  ];
+
   return (
     <div>
       {/* Page Heading */}
@@ -19,9 +32,7 @@ const DocumentsListing = () => {
           </span>
         </p>
         <div className="flex items-center gap-2">
-          <LinkButton to={"create"} primary={true} leftIcon={<IoMdAddCircle />}>
-            New Document
-          </LinkButton>
+          
           <Search placeholder={"Search Files"} />
         </div>
       </div>
@@ -29,15 +40,16 @@ const DocumentsListing = () => {
       {/* Filter */}
       <div>
         <Selector
-          label={"Files"}
-          placeholder={"File type"}
-          options={fileTypeOptions}
+          className={"w-fit"}
+          label={"Folders"}
+          placeholder={"Select Services"}
+          options={servicesOptions}
         />
       </div>
       {/* Folders and files */}
-      <div className="grid grid-cols-1 ">
-        {Array.from({ length: 5 }, () => (
-          <Folder />
+      <div className="py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        {folders.map((data, index) => (
+          <Folder key={index} data={data} />
         ))}
       </div>
     </div>
@@ -46,11 +58,53 @@ const DocumentsListing = () => {
 
 export default DocumentsListing;
 
-const Folder = () => {
+const Folder = ({ data }) => {
+  const [folder, setFolder] = useState(false);
+
   return (
-    <div className="bg-[#F2F2F2] rounded">
-      <img src="/icons/documents/folder.svg" alt="folder-icon" />
-      <div></div>
-    </div>
+    <>
+      {!folder ? (
+        <div
+          onClick={() => setFolder(true)}
+          className="relative bg-[#F2F2F2] px-4 py-2 flex justify-between items-center gap-4 border rounded"
+        >
+          <div className="flex items-center gap-4 ">
+            <img src="/icons/documents/folder.svg" alt="folder-icon" />
+            <p className="font-semibold text-xs">{data.name}</p>
+          </div>
+          <button>
+            <img src="/icons/documents/three-dots.svg" alt="folder-icon" />
+          </button>
+        </div>
+      ) : (
+        <>
+          {data?.files.map((file, index) => (
+            <div key={index} className="file-item">
+              {file.type === "image" && (
+                <div className="image-file">
+                  <img src={file.url} alt={file.name} className="w-full h-auto" />
+                  <p>{file.name}</p>
+                </div>
+              )}
+              {file.type === "pdf" && (
+                <div className="pdf-file">
+                  <iframe src={file.url} width="100%" height="200px" title={file.name}></iframe>
+                  <p>{file.name}</p>
+                </div>
+              )}
+              {file.type === "video" && (
+                <div className="video-file">
+                  <video width="100%" height="auto" controls>
+                    <source src={file.url} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                  <p>{file.name}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
+    </>
   );
 };
--- src/pages/services/listing/index.jsx
+++ src/pages/services/listing/index.jsx
@@ -1,53 +1,81 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../../../components/buttons";
 import Insight from "../../../components/insight/insight";
 import { MainTab } from "../../../pages/services/components/tabs/mainTab";
 import ServicesCard from "../../../pages/services/listing/components/services-card";
 import { servicesListing } from "../../../database";
 import Filtertab from "../../../pages/services/components/tabs/filterTab";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
-import {getUserServicesCatagory,getUserServicesSubCatagory,getUserServices,updateServiceWishlist,removeServiceWishlist} from '../../../redux/actions/servicesListing-action';
+import {
+  getUserServicesCatagory,
+  getUserServicesSubCatagory,
+  getUserServices,
+  updateServiceWishlist,
+  removeServiceWishlist,
+} from "../../../redux/actions/servicesListing-action";
 import toast from "react-hot-toast";
 const ServicesListing = () => {
-  const dispatch =useDispatch();
+  const dispatch = useDispatch();
   const { servicesMainTab } = useSelector((state) => state.app);
-  const { category,subCategory,page,limit,list,wishList:{loading,error}} = useSelector((state) => state.service);
-  const location=useLocation();
+  const {
+    category,
+    subCategory,
+    page,
+    limit,
+    list,
+    wishList: { loading, error },
+  } = useSelector((state) => state.service);
+  const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const searchValue = queryParams.get('search');
-  const [isSubmit,setIsSubmit]=useState(false);
-  useEffect(()=>{
+  const searchValue = queryParams.get("search");
+  const [isSubmit, setIsSubmit] = useState(false);
+  useEffect(() => {
     dispatch(getUserServicesCatagory({}));
-  },[])
-  useEffect(()=>{
-    dispatch(getUserServicesSubCatagory({categoryId:category?.selectedCategory?.categoryId}));
-  },[category.selectedCategory])
-  useEffect(()=>{
-    if(category?.selectedCategory&&subCategory?.selectedSubCategory){
-      dispatch(getUserServices({categoryId:category?.selectedCategory?._id
-        ,subCategoryId:subCategory?.selectedSubCategory?._id
-        ,page,limit}));
+  }, []);
+  useEffect(() => {
+    dispatch(
+      getUserServicesSubCatagory({
+        categoryId: category?.selectedCategory?.categoryId,
+      })
+    );
+  }, [category.selectedCategory]);
+  useEffect(() => {
+    if (category?.selectedCategory && subCategory?.selectedSubCategory) {
+      dispatch(
+        getUserServices({
+          categoryId: category?.selectedCategory?._id,
+          subCategoryId: subCategory?.selectedSubCategory?._id,
+          page,
+          limit,
+        })
+      );
     }
-  },[category.selectedCategory,subCategory.selectedSubCategory])
-  useEffect(()=>{
-      dispatch(getUserServices({categoryId:category?.selectedCategory?._id
-        ,subCategoryId:subCategory?.selectedSubCategory?._id
-        ,page,limit,query:searchValue}));
-  },[searchValue])
-  useEffect(()=>{
-    if(isSubmit&&!loading){
-      toast.success(error)
+  }, [category.selectedCategory, subCategory.selectedSubCategory]);
+  useEffect(() => {
+    dispatch(
+      getUserServices({
+        categoryId: category?.selectedCategory?._id,
+        subCategoryId: subCategory?.selectedSubCategory?._id,
+        page,
+        limit,
+        query: searchValue,
+      })
+    );
+  }, [searchValue]);
+  useEffect(() => {
+    if (isSubmit && !loading) {
+      toast.success(error);
     }
-},[loading])
-  let onClickWishList=(service)=>{
-    setIsSubmit(true)
-      if(service?.wishlistCount){
-        dispatch(removeServiceWishlist({serviceId:service?._id}))
-      }else{
-        dispatch(updateServiceWishlist({serviceId:service?._id}))
-      }
-  }
+  }, [loading]);
+  let onClickWishList = (service) => {
+    setIsSubmit(true);
+    if (service?.wishlistCount) {
+      dispatch(removeServiceWishlist({ serviceId: service?._id }));
+    } else {
+      dispatch(updateServiceWishlist({ serviceId: service?._id }));
+    }
+  };
   return (
     <section className="flex sm:flex-row flex-col gap-4 sm:pt-6 pt-3 bg-white">
       <div className="flex flex-col sm:w-[70%]">
@@ -59,14 +87,22 @@ const ServicesListing = () => {
                 Service Category
               </p>
               <Filtertab />
-              <ServicesCard data={list} onClick={(service)=>onClickWishList(service)}/>
+              <ServicesCard
+                data={list}
+                onClick={(service) => onClickWishList(service)}
+              />
             </>
           ) : (
             <>
-              <ServicesCard data={list} onClick={(service)=>onClickWishList(service)}/>
-              {list&&list.length!=0&&<div className="mt-10 flex justify-center">
-                <Button primary={true}>Load More </Button>
-              </div>}
+              <ServicesCard
+                data={list}
+                onClick={(service) => onClickWishList(service)}
+              />
+              {list && list.length != 0 && (
+                <div className="mt-10 flex justify-center">
+                  <Button primary={true}>Load More </Button>
+                </div>
+              )}
             </>
           )}
         </div>
