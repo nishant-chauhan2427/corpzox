e48a12ccd6753c281f1b0f36e7810f4deba84554
Rahul Mahto <Rahulmahto837@gmail.com>
2024-11-20 10:28:59 +0530

    Ui fixed

--- src/components/offers/index.jsx
+++ src/components/offers/index.jsx
@@ -7,7 +7,7 @@ const Offers = () => {
       <div className="flex flex-col gap-4 ">
         <div className="flex justify-between items-center pb-2">
           <p className="font-semibold text-[16px] text-[#004BBC]">Offers</p>
-          <p className="font-semibold text-xs text-[#828282]">View All</p>
+          <p className="font-semibold text-xs text-[#828282]">View Al</p>
         </div>
         <div className="h-[0.5px] bg-gradient-to-r  from-[#E0E1E2] via-[#E0E1E2] to-[#E0E1E2]"></div>
         {offers.map((offers, index) => (
