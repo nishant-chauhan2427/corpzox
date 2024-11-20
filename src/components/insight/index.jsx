c813aeaa2ed21619c5e09254e1867a4c23637f4a
Rahul Mahto <Rahulmahto837@gmail.com>
2024-11-20 18:46:19 +0530

    create payment page

--- src/components/insight/index.jsx
+++ src/components/insight/index.jsx
@@ -7,7 +7,7 @@ export const Insight = () => {
       <div className="flex flex-col gap-4">
         <div className="flex justify-between items-center pb-2">
           <p className="font-semibold text-[16px] text-[#004BBC]">Insights</p>
-          <p className="font-semibold text-xs text-[#828282]">View All</p>
+          <Link className="font-semibold text-xs text-[#828282]">View All</Link>
         </div>
         <div className="h-[0.5px] bg-gradient-to-r from-[#E0E1E2] via-[#E0E1E2] to-[#E0E1E2]"></div>
         {insightBlog.map((blog, index) => (
