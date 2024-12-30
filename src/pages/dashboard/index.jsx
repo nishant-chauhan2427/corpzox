9f6f9507aee93af83126610a9351eb27290f1fdb
Rahul mahto <rahulmahto837@gmail.com>
2024-12-30 18:42:48 +0530

    checking pull request

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -141,7 +141,6 @@ const Dashboard = () => {
           {/* <Advertisement /> */}
         </div>
         {businessLoading ? (<BusinessCardShimmer />) : (<Business data={business?.list} total={business?.totalPage} />)}
-
         {isRecommendedServiceLoading ? (
           <div className="flex flex-row gap-2">
             {Array.from({ length: 2 }, (_, index) => (
